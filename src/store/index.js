cb1a915aa20d5924fb64a7c4f90b0aa63550ec3a
Zhuk7777 <93807982+Zhuk7777@users.noreply.github.com>
2023-05-08 15:43:27 +0300

    Signed-off-by: Zhuk7777 <93807982+Zhuk7777@users.noreply.github.com>
    	modified:   src/store/index.js

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,8 @@
 import {configureStore} from '@reduxjs/toolkit'
-import { useReducer } from 'react'
+import userReducer from './slices/userSlice'
 
 export const store = configureStore({
     reducer: {
-        user: useReducer,
+        user: userReducer,
     }
 })
