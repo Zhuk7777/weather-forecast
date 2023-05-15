70ee718643cf40699a694ccf6789ec470a428e56
Zhuk7777 <93807982+Zhuk7777@users.noreply.github.com>
2023-05-15 21:05:35 +0300

    Signed-off-by: Zhuk7777 <93807982+Zhuk7777@users.noreply.github.com>
    	modified:   "src/components/\321\201ontrolPanel/ChatControlPanel.jsx"
    	modified:   "src/components/\321\201ontrolPanel/ControlPanel.module.css"

--- src/components/сontrolPanel/ChatControlPanel.jsx
+++ src/components/сontrolPanel/ChatControlPanel.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from './ControlPanel.module.css';
 import { useNavigate } from "react-router-dom";
 import { getAuth, signOut } from "firebase/auth";
@@ -7,6 +7,10 @@ import { getAuth, signOut } from "firebase/auth";
 const ChatControlPanel = () => {
 
     const navigate = useNavigate()
+    const [user, setUser] = useState({
+      name: '',
+      email: '',
+    })
 
     const clickWeather = () => {
         navigate('/')
@@ -22,11 +26,39 @@ const ChatControlPanel = () => {
         navigate('/')
       })
     }
+
+    const showUserInfo = () => {
+      const auth = getAuth()
+      if(auth.currentUser)
+      {
+        setUser({
+          name: auth.currentUser.displayName,
+          email: auth.currentUser.email,
+        })
+      }
+    }
+
+    const hideUserInfo = () => {
+      setUser({
+        name: '',
+        email: '',
+      })
+    }
+
   return (
     <div className={classes.controlPanel}>
         <button className={classes.button} onClick={() => clickWeather()}>Погода</button>
-        <button className={classes.button} onClick={() => changeUser()}>Сменить пользователя</button>
+        <button className={classes.button} onClick={() => changeUser()} onMouseOver={() => showUserInfo()} onMouseLeave={() => hideUserInfo()}>Сменить пользователя</button>
         <button className={classes.button} onClick={() => exit()}>Выйти</button>
+        {
+          user.name?
+          <div className={classes.userInfo}>
+            <span>Текущий пользователь</span>
+            <span>{user.name}</span>
+            <span>{user.email}</span>
+          </div>:
+          <div></div>
+        }
     </div>
   )
 }
